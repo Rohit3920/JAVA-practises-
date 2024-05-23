ffcdc66c76aae0e5332e48162ce7eff1f0e8ca36
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-23 15:38:26 +0530

    If, IF-else :FInding even or odd number using Tornary Operator in JAVA

--- IfElse.java
+++ IfElse.java
@@ -8,20 +8,23 @@ public class IfElse {
         System.out.println("Enter number : ");
         int a = sc.nextInt();
 
-        if (a <= 9) {
-            System.out.println("this number is single Digit " + a);
-            if (a % 2 == 0) {
-                System.out.println("this number is even " + a);
-            } else {
-                System.out.println("this number is odd " + a);
-            }
-        } else {
-            System.out.println("this number is more then one Digit " + a);
-            if (a % 2 == 0) {
-                System.out.println("this number is even " + a);
-            } else {
-                System.out.println("this number is odd " + a);
-            }
-        }
+        String num = a%2 == 0 ? "Even": "odd";
+            System.out.println("this number is " + num +" -> " + a);
+
+        // if (a <= 9) {
+        //     System.out.println("this number is single Digit " + a);
+        //     if (a % 2 == 0) {
+        //         System.out.println("this number is even " + a);
+        //     } else {
+        //         System.out.println("this number is odd " + a);
+        //     }
+        // } else {
+        //     System.out.println("this number is more then one Digit " + a);
+        //     if (a % 2 == 0) {
+        //         System.out.println("this number is even " + a);
+        //     } else {
+        //         System.out.println("this number is odd " + a);
+        //     }
+        // }
     }
 }
