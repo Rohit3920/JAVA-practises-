a4a87ee0867f0c014c247e6811bd9f390392ca6c
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-23 19:18:14 +0530

    For Loop : Print 1 to 10  number table using Nested for loop in JAVA

--- ForLoop.java
+++ ForLoop.java
@@ -1,15 +1,22 @@
 
-import java.util.Scanner;
 
 public class ForLoop {
 
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.println("For Loop ");
-        System.out.println("Enter number to print table ");
-        int num = obj.nextInt();
+        // Scanner obj = new Scanner(System.in);
+        // System.out.println("For Loop ");
+        // System.out.println("Enter number to print table ");
+        // int num = obj.nextInt();
+        // for (int i = 1; i <= 10; i++) {
+        //     System.out.println(i * num);
+        // }/
+
+
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i * num);
+            for(int j =1; j <= 10; j++){
+                System.out.print(j*i +"  ");
+            }
+            System.out.print("\n");
         }
     }
 }
