814f88280604197c9b6100b1de0e2634941cfbaf
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-23 14:59:04 +0530

    If, IF-else : implimentation of If and If-else statemant in JAVA

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] para){
+        int a = 20;
+        int b = 20;
+        int res = a - b;
+        if(a-b == 10)
+        System.out.println("Subsrtraction for if = "+ res);
+        else
+        System.out.println("Subsrtraction for else = "+ res);
+    }
+}
