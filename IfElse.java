f506c4ad315b9b537c21baa885d203e49dd9ab5a
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-23 15:21:23 +0530

    If, IF-else :Finding digit in the given number using if-else ladder in JAVA

--- IfElse.java
+++ IfElse.java
@@ -1,14 +1,20 @@
+
 import java.util.Scanner;
 
 public class IfElse {
-    public static void main(String[] para){
+    public static void main(String[] para) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Year : ");
+        System.out.println("Enter finding digit number : ");
         int a = sc.nextInt();
 
-        if(a%4 == 0)
-        System.out.println("this is leaf Year " + a);
-        else
-        System.out.println("this is not leaf Year " + a);
+        if (a <= 9) {
+            System.out.println("this number is single Digit " + a); 
+        }else if (a <= 99 && a >= 10) {
+            System.out.println("this number is two Digit " + a); 
+        }else if (a <= 999 && a >= 100) {
+            System.out.println("this number is three Digit " + a); 
+        }else {
+            System.out.println("this number is more then tree Digit " + a);
+        }
     }
 }
