7d5fcfd30b8c636febd1866db934a0bc8596e923
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-24 12:12:19 +0530

    While and Do While LOOP : print the number using while loop in JAVA

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+
+public class WhileLoop {
+
+    public static void main(String[] para) {
+        System.out.println("While Loop");
+        int n = 0;
+        while (n < 10) {
+            n++;
+            System.out.println(n);
+        }
+    }
+}
