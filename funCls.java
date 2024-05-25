fc910176b968b29ecc2a627ad2c47b2ebeae1a2c
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-25 20:27:59 +0530

    Function : Public fucntion called

---
+++ funCls.java
@@ -0,0 +1,13 @@
+public class funCls {
+    public static void main(String[] args){
+        System.out.println("\n\nFunction in JAVA \n------------------------------------------------------------------");
+        funCls f1 = new funCls();
+        f1.getData();
+
+        System.out.println("\n\n");
+    }
+
+    public void getData(){
+        System.out.println("Public function Called");
+    }
+}
