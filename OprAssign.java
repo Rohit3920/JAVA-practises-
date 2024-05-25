9ea4522a3f2da93bd5daee58269f2d186e017a88
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-25 18:28:12 +0530

    Operator : Perform ASSIGNMENT Operation

---
+++ OprAssign.java
@@ -0,0 +1,15 @@
+
+public class OprAssign {
+
+    public static void main(String[] args) {
+        int a = 0;
+
+        System.out.println("After value : "+ a + "   ---  Simple assignment operator : " + (a= 10));
+        System.out.println("After value : "+ a + "   ---  Simple assignment operator : " + (a+= 10));
+        System.out.println("After value : "+ a + "   ---  Simple assignment operator : " + (a-= 10));
+        System.out.println("After value : "+ a + "   ---  Simple assignment operator : " + (a*= 10));
+        System.out.println("After value : "+ a + "   ---  Simple assignment operator : " + (a/= 10));
+        System.out.println("After value : "+ a + "   ---  Simple assignment operator : " + (a%= 10));
+
+    }
+}
