875f5c421d7e808d7eaeb13155e6e9001e517d53
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-25 18:21:57 +0530

    Operator : Perform COMPARISM Operation

---
+++ OprCompri.java
@@ -0,0 +1,16 @@
+public class OprCompri {
+    public static void main(String[] arge){
+        int a = 10; 
+        int b = 20;
+
+        System.out.println("\n\nComparism for : " + a +" and "+ b);
+    System.out.println("\nEqual to -> " + (a == b));
+    System.out.println("Not Equal to -> " + (a != b));
+    System.out.println("Less Then -> " + (a < b));
+    System.out.println("Greater then -> " + (a > b));
+    System.out.println("Less then or Equal to -> " + (a <= b));
+    System.out.println("Greater then or Equal to -> " + (a >= b));
+System.out.println("\n\n\n");
+
+    }
+}
