bd803a73d9518bd88dba1f78901573caca8a59db
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-25 18:04:28 +0530

    Operator : Perform ARITHMATIC Operation

---
+++ OprArth.java
@@ -0,0 +1,12 @@
+public class OprArth {
+    public static void main(String[] args){
+        int a = 12;
+        int b = 6;
+
+        System.out.println("Addition : " + a+b);
+        System.out.println("Substraction : " + (a-b));
+        System.out.println("Multiplication : " + a*b);
+        System.out.println("Division : " + a/b);
+        System.out.println("Modulers : " + a%b);
+    }
+}
