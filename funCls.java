a69c8e9ddb63e8527252114a31d3e33c478a8e0e
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-25 20:41:22 +0530

    Function : user throught calculate simple interest using function with parameters

--- funCls.java
+++ funCls.java
@@ -1,11 +1,16 @@
-
+import java.util.Scanner;
 public class funCls {
 
     public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
         System.out.println("\n\nFunction in JAVA \n------------------------------------------------------------------");
-        int amount = 10000;
-        int time = 2;
-        int rate = 12;
+        System.out.println("Amount");
+        int amount = obj.nextInt();
+        System.out.println("time");
+        int time =obj.nextInt();
+        System.out.println("rate");
+
+        int rate = obj.nextInt();
         simpleInterest(amount, time, rate);
         System.out.println("\n\n");
     }
