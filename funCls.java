0a53000ca3f2cd22422d83d8302177f6347744bf
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-05-25 20:31:16 +0530

    Function : Public fucntion convert static function

--- funCls.java
+++ funCls.java
@@ -1,13 +1,15 @@
 public class funCls {
     public static void main(String[] args){
         System.out.println("\n\nFunction in JAVA \n------------------------------------------------------------------");
-        funCls f1 = new funCls();
-        f1.getData();
+        // funCls f1 = new funCls();
+        // f1.getData();
+
+        getData();
 
         System.out.println("\n\n");
     }
 
-    public void getData(){
-        System.out.println("Public function Called");
+    public static  void getData(){
+        System.out.println("Static function Called");
     }
 }
