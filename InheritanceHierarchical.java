134ae55c531d7c91958a161b2963dd89bc8f5634
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-02 20:48:55 +0530

    Inheritance in Hierarchical : create three classes and extends classes

---
+++ InheritanceHierarchical.java
@@ -0,0 +1,29 @@
+public class InheritanceHierarchical {
+    public static void main(String[] args) {
+        System.out.println("Hierarchical Inheritance in java");
+
+        child c1 = new child();
+        c1.getGrandFather();
+        c1.getFather();
+        c1.getChild();
+
+    }
+}
+
+class grandFather{
+    void getGrandFather(){
+        System.out.println("call grandfather class");
+    }
+}
+
+class father extends grandFather{
+    void getFather(){
+        System.out.println("Call father class");
+    }
+}
+
+class child extends father{
+    void getChild(){
+        System.out.println("Call child class");
+    }
+}
