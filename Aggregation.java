8d5c91da9e33c5cf419d6c87f157761bd82b0f56
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-05 18:28:43 +0530

    Aggregation in java : Convert simple form to Aggregation form

--- Aggregation.java
+++ Aggregation.java
@@ -3,8 +3,24 @@ public class Aggregation {
     public static void main(String[] args) {
         System.out.println("this is an Aggregation in java");
 
-        Student s1 = new Student("Rohit", 32, "Paroliwadi", "Ajara", "Maharashtra", "INDIA");
+        //create object for address class and pass as a parameter in student object
+        Address adrs1 = new Address("Paroliwadi", "Ajara", "Maharashtra", "INDIA");
+        Student s1 = new Student("Rohit", 32, adrs1);
         s1.studDisplay();
+    }
+}
+
+class Address{
+    String villege;
+    String city;
+    String state;
+    String country;
+
+    Address(String villege, String city, String state, String country){
+        this.villege = villege;
+        this.city = city;
+        this.state = state;
+        this.country = country;
     }
 }
 
@@ -12,27 +28,26 @@ class Student {
 
     String name;
     int rollNo;
-    String villege;
-    String city;
-    String state;
-    String country;
 
-    Student(String name, int rollNo, String villege, String city, String state, String country) {
+    //Address class as user define datatype to use variable and access this class variable in Student class
+    Address adrs;
+
+
+    Student(String name, int rollNo, Address adrs) {
         this.name = name;
         this.rollNo = rollNo;
-        this.villege = villege;
-        this.city = city;
-        this.state = state;
-        this.country = country;
+        this.adrs = adrs;
     }
 
     void studDisplay() {
         System.out.println("-----Student Datails----- \n\tName : " + name);
         System.out.println("\tRoll No. : " + rollNo);
-        System.out.println("\tVillege : " + villege);
-        System.out.println("\tCity : " + city);
-        System.out.println("\tState : " + state);
-        System.out.println("\tCountry : " + country);
+
+        //access the Address variable using dot operator and show the result
+        System.out.println("\tVillege : " + adrs.villege);
+        System.out.println("\tCity : " + adrs.city);
+        System.out.println("\tState : " + adrs.state);
+        System.out.println("\tCountry : " + adrs.country);
         System.out.println("-----------------------------------");
     }
 }
