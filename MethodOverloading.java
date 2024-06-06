b1e6fe4376d81142cf4eb3c8b97d4f7bf542b5b5
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-06 15:05:14 +0530

    Method or Function overloading : implement the function overloading with inheritance wtih different parameters

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -4,31 +4,27 @@ public class MethodOverloading {
     public static void main(String[] args) {
         System.out.println("Method or Function Overloading in java");
 
-        User u = new User();
-        u.getData();
-        u.getData("this is method overloading means function overloading");
-        u.getData("this is method overloading", 25);
-        u.getData("this is method overloading", "this is function overloading");
-
-
+        Customer c = new Customer();
+        c.login();
+        c.login("ROHIT ");
+        c.login(12345678);
     }
 }
 
 class User{
-    void getData(){
-        System.out.println("Ready to getting the data");
+    void login(){
+        System.out.println("User login successfully");
     }
+}
 
-    void getData(String data){
-        System.out.println("getting a data for : " + data);
+class Emp extends User{
+    void login(String name){
+        System.out.println(name + " login successfully");
     }
-    
-    void getData(String data, int NoOfData){
-        System.out.println("getting a data for : " + data + " for number of data : " + NoOfData);
-    }
-
-    void getData(String data1, String data2){
-        System.out.println("getting a data for 1 : " + data1 + "\n and \ngetting a data for 2 : " + data2);
+}
+class Customer extends Emp{
+    void login(int password){
+        System.out.println("Customer login successfully");
     }
 }
 
