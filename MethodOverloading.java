0796710fb433480af738ce879f9440d59508a660
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-06 14:55:04 +0530

    Method or Function overloading : implement the function overloading wtih different parameters

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -7,7 +7,8 @@ public class MethodOverloading {
         User u = new User();
         u.getData();
         u.getData("this is method overloading means function overloading");
-        u.getData(20);
+        u.getData("this is method overloading", 25);
+        u.getData("this is method overloading", "this is function overloading");
 
 
     }
@@ -22,8 +23,12 @@ class User{
         System.out.println("getting a data for : " + data);
     }
     
-    void getData(int NoOfData){
-        System.out.println("getting data in number is : " + NoOfData);
+    void getData(String data, int NoOfData){
+        System.out.println("getting a data for : " + data + " for number of data : " + NoOfData);
+    }
+
+    void getData(String data1, String data2){
+        System.out.println("getting a data for 1 : " + data1 + "\n and \ngetting a data for 2 : " + data2);
     }
 }
 
