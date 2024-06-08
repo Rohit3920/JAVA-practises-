06ffbc43c3c674df11c0645e92e552a67185a517
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-08 22:34:56 +0530

    Exception Handling - throw and throws : Occur unwanted error uwsing throw method

---
+++ throwThrows.java
@@ -0,0 +1,13 @@
+public class throwThrows {
+    public static void main(String[] args) {
+        System.out.println("Exception Handling : throw and throws : ");
+
+        try {
+            int no = 10;
+            System.out.println(no/2);
+            throw new ArithmeticException("Text");
+        } catch (ArithmeticException err) {
+            System.out.println("Error : " + err);
+        }
+    }
+}
