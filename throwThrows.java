d168a81b3e049441cb1192882cfff2d4d08e7a41
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-08 22:39:33 +0530

    Exception Handling - throw and throws : Occur unwanted error uwsing throws method in function signature

--- throwThrows.java
+++ throwThrows.java
@@ -2,12 +2,24 @@ public class throwThrows {
     public static void main(String[] args) {
         System.out.println("Exception Handling : throw and throws : ");
 
+        // try {
+        //     int no = 10;
+        //     System.out.println(no/2);
+        //     throw new ArithmeticException("Text");
+        // } catch (ArithmeticException err) {
+        //     System.out.println("Error : " + err);
+        // }
+
         try {
-            int no = 10;
-            System.out.println(no/2);
-            throw new ArithmeticException("Text");
+            checkAge();
         } catch (ArithmeticException err) {
             System.out.println("Error : " + err);
         }
     }
+
+    public static void checkAge() throws ArithmeticException{
+        System.out.println(10/0);
+    }
 }
+
+
