c77b76611975e515ce15a7d08419b6eb3d22b9cb
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-09 18:52:04 +0530

    File handling : Create multiple file and write the content using loop with try catch 'Exception Handling' in java

--- File Handling/CreateMultiFile.java
+++ File Handling/CreateMultiFile.java
@@ -1,15 +1,20 @@
 
-import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class CreateMultiFile{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
         System.out.println("Cerate multiple files and write content on file in java");
 
+        try {
         for (int i = 0; i < 10; i++) {
-            String F_nm = "File Handling/MultipleFiles/SampleFile"+i+".txt";
-            File files = new File(F_nm);
-            files.createNewFile();
+            String F_nm = "File Handling/MultipleFiles/SampleFiles"+i+".txt";
+            FileWriter filesWrite = new FileWriter(F_nm);
+            filesWrite.write("this is file for "+F_nm+" \n\t This is content for file");
+            System.out.println(F_nm +" file write successfully!!!");
+        }
+        } catch (IOException e) {
+            System.out.println("Error : "+e);
         }
     }
 }
