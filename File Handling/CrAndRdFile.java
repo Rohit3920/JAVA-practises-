d8fec97289d290d21ead6972b2403b084c262259
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-09 17:07:50 +0530

    File Handling : Created file to write a text for file using try catch 'Excerption handling' in java

--- File Handling/CrAndRdFile.java
+++ File Handling/CrAndRdFile.java
@@ -1,11 +1,23 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.FileWriter;
 
 public class CrAndRdFile {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Create file using throws 'Excerption handling' in java" );
+    // public static void main(String[] args) throws IOException {
+    //     System.out.println("Create file using throws 'Excerption handling' in java" );
 
-        File file = new File("File Handling/sample.txt");
-        file.createNewFile();
+    //     File file = new File("File Handling/sample.txt");
+    //     file.createNewFile();
+    // }
+
+
+    public static void main(String[] args) {
+        System.out.println("Createed file to Write a text for file using try catch 'Excerption handling' in java" );
+
+        try {
+            FileWriter writer = new FileWriter("File Handling/sample.txt");
+            writer.write("This is a sample file for file handling in java \n\twrite a file using file handling");
+            writer.close();
+        } catch (Exception err) {
+            System.out.println("Error : " + err);
+        }
     }
 }
