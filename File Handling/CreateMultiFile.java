5ee971389cb74d72cb8835e0b03335ae942d4e55
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-09 18:55:09 +0530

    File handling : Delete multiple file using loop with try catch 'Exception Handling' in java

--- File Handling/CreateMultiFile.java
+++ File Handling/CreateMultiFile.java
@@ -1,20 +1,20 @@
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
 
-public class CreateMultiFile{
+public class CreateMultiFile {
+
     public static void main(String[] args) {
         System.out.println("Cerate multiple files and write content on file in java");
 
         try {
-        for (int i = 0; i < 10; i++) {
-            String F_nm = "File Handling/MultipleFiles/SampleFiles"+i+".txt";
-            FileWriter filesWrite = new FileWriter(F_nm);
-            filesWrite.write("this is file for "+F_nm+" \n\t This is content for file");
-            System.out.println(F_nm +" file write successfully!!!");
-        }
-        } catch (IOException e) {
-            System.out.println("Error : "+e);
+            for (int i = 0; i < 10; i++) {
+                String F_nm = "File Handling/MultipleFiles/SampleFiles" + i + ".txt";
+                File DelFile = new File(F_nm);
+                DelFile.delete();
+                System.out.println(F_nm + " file Deleted successfully!!!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error : " + e);
         }
     }
 }
