4b5d11efe6c39d3213a9e61751ed2673049216a9
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2024-06-17 14:23:46 +0530

    JDBC in MySQL : Update database in XAMPP Server to change the code for main.java file

--- DB-Connection/src/Main.java
+++ DB-Connection/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     }
 
     public static Connection getConnection() {
-        try{
+        try {
             String driver = "com.mysql.jdbc.Driver";
             String dbURL = "jdbc:mysql://localhost/e-comm";
             String userNm = "root";
@@ -23,35 +23,36 @@ public class Main {
             Connection conn = DriverManager.getConnection(dbURL, userNm, password);
             System.out.println("Database connected successfully");
             return conn;
-        }catch (Exception err){
+        } catch (Exception err) {
             System.out.println("Connection error : " + err);
         }
         return null;
     }
 
-    public static void getDate(){
+    public static void getDate() {
         try {
             Statement statement = getConnection().createStatement();
             ResultSet result = statement.executeQuery("Select * from products");
-            while(result.next()){
-                System.out.println(result.getString("name") +
-                        " " + result.getString("country"));
+            while (result.next()) {
+                System.out.println(result.getString("id")
+                        + " " + result.getString("name") + " "
+                        + " " + result.getString("country"));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error : " + e);
         }
     }
 
-    public static void insertData(){
-        try{
+    public static void insertData() {
+        try {
             Statement statement = getConnection().createStatement();
-            int result = statement.executeUpdate("insert into products(name, country) values('dhanaraj','IND')");
-            if(result == 1) {
+            int result = statement.executeUpdate("insert into products(id, name, country) values(2,'ROHIT','INDIA')");
+            if (result == 1) {
                 System.out.println("Inserted data successfully");
-            }else {
-                  System.out.println("insert data failed");
+            } else {
+                System.out.println("insert data failed");
             }
-        }catch(Exception err){
+        } catch (Exception err) {
             System.out.println("Error : " + err);
         }
     }
